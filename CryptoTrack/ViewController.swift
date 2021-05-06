9807ce80d281f02ef6ecbdc774a56a0368ca18ea
MasashiAkahori <il_pendolino.masa@docomo.ne.jp>
2021-05-07 07:34:22 +0900

    Initial Commit

---
+++ CryptoTrack/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CryptoTrack
+//
+//  Created by 赤堀雅司 on 7/5/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
